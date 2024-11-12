e623dd9145115eee4c11de73c9fe873b163890b8
soybean15 <marlonpadilla1593@gmail.com>
2024-11-12 13:36:24 +0800

    Update - fixed project structure

---
+++ app/Packages/RichTextEditor/src/Components/RichTextEditor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Packages\RichTextEditor\src\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class RichTextEditor extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public $model,
+        public $key,
+        public $value,
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('rich-text-editor::rich-text-editor');
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,8 @@
 <?php
 
+
+
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\RichTextEditorServiceProvider::class,
+    App\Packages\RichTextEditor\src\RichTextEditorServiceProvider::class
 ];
